26d0c6c99b34057dc4ba19acf9e9dd98b9d651e4
FaziletKosure <faziletkosure1@gmail.com>
2020-12-05 16:53:48 +0100

    add

---
+++ node_f/index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+require("dotenv").config();
+const port = process.env.port || 5001;
+const host = "localhost";
+const app = express();
+//middleware
+app.use((req, res, next) => {
+  console.log("middleware: 1");
+  // some jobs
+  next();
+});
+app.use((req, res, next) => {
+  console.log("middleware: 2");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("middleware: 3");
+  if (true) {
+    res.send("OK");
+  }
+});
+app.get("/user", (req, res) => {});
+app.listen(port, host, () => {
+  console.log("I'm listening on http://%s:%s", host, port);
+});
+
+
+///Logger morgan
+
+
+// const express = require("express");
+// require("dotenv").config();
+// const logger = require("morgan");
+// const port = process.env.port || 5001;
+// const host = "localhost";
+// const app = express();
+// //middleware
+// app.use(logger("dev"));
+// app.get("/user", (req, res) => {
+//   res.send("User details");
+// });
+// app.listen(port, host, () => {
+//   console.log("I'm listening on http://%s:%s", host, port);
+// });
+
+
+
+// Middlewarelere arasi data gonderme
+
+// app.use((req, res, next) => { //Authentication
+//     console.log("Auth");
+//     // some jobs
+//     res.isAuth = false;
+//     next();
+//   });
+//   app.use((req, res, next) => {
+//     console.log("Controller // db");
+//    if (res.isAuth) {
+//        console.log("Private data")
+//    } else {
+//        console.log("not auth")
+//    }
+//     next();
+//   });
+//   app.use((req, res, next) => {
+//     console.log("middleware: 3");
+//     if (true) {
+//       res.send("send");
+//     }
+//   });
+
+
+// COOKIES-PARSER
+
+// const express = require("express");
+// require("dotenv").config();
+// var cookieParser = require("cookie-parser");
+// const port = process.env.port || 5001;
+// const host = "localhost";
+// const app = express();
+// //middleware
+// app.use(cookieParser());
+// app.use((req, res, next) => {
+//   console.log("Cookies: ", req.signedCookies);
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log("Controller // db");
+//   if (res.isAuth) {
+//     console.log("Private data");
+//   } else {
+//     console.log("not auth");
+//   }
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log("middleware: 3");
+//   if (true) {
+//     res.send("send");
+//   }
+// });
+// app.get("/user", (req, res) => {});
+// app.listen(port, host, () => {
+//   console.log("I'm listening on http://%s:%s", host, port);
+// });
